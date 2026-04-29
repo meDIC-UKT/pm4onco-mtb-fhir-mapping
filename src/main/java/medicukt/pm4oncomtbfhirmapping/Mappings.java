f0166c2b7f748ffd7207c5faa18ad1b59f9df95b
Masha Lenuzzi <masa.lenuzzi@uni-tuebingen.de>
2026-04-29 15:03:39 +0200

    initialize project with base structure and configuration

---
+++ src/main/java/medicukt/pm4oncomtbfhirmapping/Mappings.java
@@ -0,0 +1,9 @@
+package medicukt.pm4oncomtbfhirmapping;
+
+public class Mappings {
+    public static void main(String[] args) {
+
+        //todo: implement mapping of mtb data from dnpm to fhir
+
+    }
+}
---
+++ src/test/java/medicukt/pm4oncomtbfhirmapping/Tests.java
@@ -0,0 +1,9 @@
+package medicukt.pm4oncomtbfhirmapping;
+
+public class Tests {
+    public static void main(String[] args) {
+
+        //todo: implement mapping tests
+
+    }
+}
